1b2761785e3388cdd337bd3dd52b0422d2cfed03
LiMi-SEDC <75808911+LiMi-SEDC@users.noreply.github.com>
2021-01-11 13:05:27 +0100

    Add files via upload

---
+++ 3 homeworks/app.js
@@ -0,0 +1,62 @@
+
+// HOMEWORK 3
+
+// let cityBank = prompt(`Hello sir , do u need to take some money?`);
+
+// if (cityBank === `yes`) {
+//     let atm = prompt(`So how much money do u need?`);
+//     if (atm >= 1000) {
+//         alert(`Not enough money`);
+//     } else if (atm <= 1000) {
+//         alert(`awesome heres ur money`);
+//     } 
+// } else if (cityBank === `no`) {
+//     alert(`Cool have a nice day sir`);
+// }
+
+
+
+// HOMEWORK 2
+
+// //  conversion rate of 1 human year to 7 dog years
+// let dogsAge = 28;
+// let humansAge = 19; 
+// let conversationRate = humansAge * 7;
+// console.log(conversationRate);
+
+// let conversationRateDogToHuman = dogsAge / 7;
+// console.log(conversationRateDogToHuman);
+
+
+
+// HOMEWORK 1
+
+
+// let anNbaPlayer = {
+//     firstName: `kobe`,
+//     lastName: `bryant`,
+//     age: 41,
+//     achievements: `5x championship`,
+//     married:true,
+// };
+
+// console.log(anNbaPlayer.firstName);
+// console.log(anNbaPlayer.lastName);
+// console.log(anNbaPlayer.age);
+// console.log(anNbaPlayer.achievements);
+// console.log(anNbaPlayer.points);
+// console.log(anNbaPlayer.married);
+
+
+
+// this was just another way i tried but i dont think it worked so i just left it there ...
+
+// function anNbaPlayer() {
+//     console.log(`Kobe Bryant`);
+//     console.log(41 + " " + `years old`);
+//     console.log(`Achievements: 5× NBA champion`);
+//     console.log(`Married:` + " " + true);
+//     console.log()
+// }
+
+// anNbaPlayer();
